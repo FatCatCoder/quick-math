0a2f347e4e21223ac08ac70792c558e406df7991
FatCatCoder <christian75armand@gmail.com>
2021-11-29 18:10:00 -0500

    cc

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Circle Calculator</h1>
       <Circle />
     </div>
   )
--- src/components/circle.tsx
+++ src/components/circle.tsx
@@ -15,31 +15,63 @@ export default function circle() {
 
 
     function conversion(r: number = 0, d: number = 0, c: number = 0, a: number = 0): CircleFace[]{
-        if(r){
-            return [
-                {"name": 'radius', "value": r},
-                {"name": 'diameter', "value": 2*r},
-                {"name": 'circumference', "value": 2* Math.PI * r},
-                {"name": 'area', "value": Math.PI * r**2}
-        ]
+        let rCalc: number = r, dCalc: number = d, cCalc: number = c, aCalc: number = a;
+        
+        if((r | d | c | a) === 0 || undefined){
+            return []
         }
-        return [];
+
+        else if(r){
+            rCalc = r
+            dCalc = 2*r
+            cCalc = 2 * Math.PI * r
+            aCalc = Math.PI * r**2
+        }
+        else if(d){
+            rCalc = d/2
+            dCalc = d
+            cCalc = Math.PI * d
+            aCalc =  Math.PI * (d/2)**2
+        }
+        else if(c){
+            rCalc = c/(2*Math.PI)
+            dCalc = c/Math.PI
+            cCalc = c
+            aCalc = c**2 / (4*Math.PI)
+        }
+        else if(a){
+            rCalc = Math.sqrt(a/Math.PI)
+            dCalc = Math.sqrt((4*a)/(Math.PI))
+            cCalc = 2 * Math.sqrt(Math.PI*a)
+            aCalc = a
+        }
+        return [
+            {"name": 'radius', "value": rCalc},
+            {"name": 'diameter', "value": dCalc},
+            {"name": 'circumference', "value": cCalc},
+            {"name": 'area', "value": aCalc}
+        ];
     }
 
     const handleChange = (event: any) => {
+        let val = event.target.value;
+
         switch(event.target.name) {
             case 'radius':
-                setRadiusValue(event.target.value)
-                setSolutions(conversion(event.target.value))
+                setRadiusValue(val)
+                setSolutions(conversion(val))
               break;
             case 'diameter':
-                setDiameterValue(event.target.value)
+                setDiameterValue(val)
+                setSolutions(conversion(0, val))
               break;
             case 'circumference':
-                setCircumferenceValue(event.target.value)
+                setCircumferenceValue(val)
+                setSolutions(conversion(0, 0, val))
                 break;
             case 'area':
-                setAreaValue(event.target.value)
+                setAreaValue(val)
+                setSolutions(conversion(0, 0, 0, val))
                 break;
           }
     }
@@ -61,9 +93,11 @@ export default function circle() {
             </form>
 
             <p>Solution:</p>
-            {solutions.map((x: CircleFace, y: number) => {
-                return <p key={y}>{`${x.name} ${x.value}`}</p>
-            })}
+            {
+                solutions.map((x: CircleFace, y: number) => {
+                    return <p key={y}>{`${x.name} ${x.value}`}</p>
+                })
+            }
 
         </div>
     )
